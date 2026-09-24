5dedc0c7362d2b48e89d59358e0a5d01e451b6fa
Shaurya Pratap Singh <shaurya.pratap.singh.dev@gmail.com>
2026-09-24 16:03:09 +0530

    Time: 3 ms (14.43%), Space: 23.9 MB (55.31%) - LeetHub - Time: O(n), Space: O(1)(optimum)

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -2,15 +2,12 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int position = 0;
+
         for(int i=0; i<nums.size(); i++){
             if(nums[i]!=0){
-                nums[position] = nums[i];
+                swap(nums[position],nums[i]);
                 position++;
             }
-        }
-        while(position<nums.size()){
-                nums[position] =0;
-                position++;
         }
     }
 };
